59af088afc6bc2cdac975e25135ae6f3f09972cb
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2022-11-13 12:44:41 +0100

    Add NewDrawNumberView

---
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/impl/NewDrawNumberView.java
@@ -0,0 +1,27 @@
+package it.unibo.mvc.impl;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class NewDrawNumberView implements DrawNumberView {
+
+    @Override
+    public void setController(DrawNumberController observer) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
